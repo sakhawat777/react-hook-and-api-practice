953f4cf312dd23fb398f9e4f70e22ecb7c48557a
sakhawat777 <biplobbms@gmail.com>
2021-11-29 22:40:30 +0600

    Project Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [users, setUsers] = useState([]);
+  const [singleUser, setSingleUser] = useState({});
+  const [randomUser, setRandomUser] = useState({});
+	useEffect(() => {
+    // For Multiple User
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then((res) => res.json())
+			.then((data) => setUsers(data))
+// For Single User
+fetch('https://jsonplaceholder.typicode.com/users/1')
+.then((res) => res.json())
+.then((data) => setSingleUser(data))
+// Random User
+fetch('https://randomuser.me/api/')
+.then((res) => res.json())
+.then((data) => setRandomUser(data.results[0]))
+	}, []);
+	return <div>
+    {
+      users.map(user => <li>{user.name}</li>)
+    }
+    <h1>{singleUser.name}</h1>
+    <h2>{randomUser.gender}</h2>
+    {/* Short-Circuit= && */}
+    <h2>{randomUser.name && randomUser.name.title + " " + randomUser.name.first  + " " + randomUser.name.last}</h2>
+    {/* Nullable= ? */}
+    <h3>{randomUser.name?.title + " " + randomUser.name?.first + " " + randomUser.name?.last}</h3>
+   
+  </div>;
 }
 
 export default App;
